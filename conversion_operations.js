9b0b3f0e3eba54e277070ebdcb16990d79b30af2
saaaakshiii <147138497+saaaakshiii@users.noreply.github.com>
2024-07-19 05:52:25 +0000

    conversion_operations

---
+++ conversion_operations.js
@@ -0,0 +1,32 @@
+// let score=true;
+
+// console.log(typeof score);
+
+// let valueInScore=Number(score);
+// console.log( valueInScore);
+
+/*
+    After type conversion
+
+1. "33"--> Number
+2. "33abc"--> NaN (Not a number)
+3. true--> 1 false--> 0
+
+*/
+
+// let isLoogedIn= "Sakshi";
+
+// let value=Boolean(isLoogedIn);
+
+// console.log(value);
+
+//1--> true after boolean conversion
+//Empty string--> False
+//"Sakshi"--> True
+
+let someNumber=33;
+
+let stringNumber= String(someNumber);
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
