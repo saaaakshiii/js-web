ef9add012105a86cb103a6b6f2fa5cfb17cd6302
saaaakshiii <147138497+saaaakshiii@users.noreply.github.com>
2024-07-19 03:50:49 +0000

    datatypes

---
+++ 01_basics/data_types.js
@@ -0,0 +1,26 @@
+"use strict";
+
+// alert(3+3) //we are using node js not browser
+
+console.log(3+3)
+console.log("Sakshi")
+
+let name="Sakshi"
+let age=18
+let isLoggedIn=false
+let state;
+
+/*
+    Data Types
+1. number
+2. Big Int
+3. String
+4. Boolean
+5. Null--> Standalone Value
+6. Undefined
+7. Symbol--> for uniqueness
+
+*/
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
