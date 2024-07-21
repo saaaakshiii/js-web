d491e827ccb379402e0a2801ec38451c7ff29568
saaaakshiii <147138497+saaaakshiii@users.noreply.github.com>
2024-07-21 07:50:20 +0000

    strings

---
+++ 01_basics/strings.js
@@ -0,0 +1,18 @@
+const name="Sakshi "
+const repoCount=10
+
+console.log(name+repoCount+" Value");
+
+//USing String Interpolation
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName= new String("Candy-Crush");
+
+console.log(gameName.__proto__);//For Prototype
+
+console.log(gameName.length);
+console.log((gameName.toUpperCase()));
+console.log(gameName.charAt(3));
+console.log(gameName.indexOf('n'));
+
