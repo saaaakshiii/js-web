f2082438095d8137d008cbabaedc4ed51eb8af2e
saaaakshiii <147138497+saaaakshiii@users.noreply.github.com>
2024-10-10 16:41:04 +0000

    iife

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -1,1 +1,15 @@
 // Immediately Invoked Function Expressions (IIFE)
+// Used to remove the pollution caused by global scope varibles 
+
+(function chai(){
+    //named iife
+    console.log(`DB Connected`);   
+})();
+
+//Arrow Function
+((name) =>{
+    
+    //simple/ unnamed iife
+    console.log(`DB CONNECTED TWO, ${name}`);
+})('sakshi')
+
