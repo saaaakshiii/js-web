6992dc399199d6687b92aa12201e955ddf7103be
saaaakshiii <147138497+saaaakshiii@users.noreply.github.com>
2024-10-10 09:22:39 +0000

    objects in depth

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -2,11 +2,36 @@
 // Object.create
 //Object Literals
 
+const mySym= Symbol("key1");
+
 const jsUser={
     name1: "Sakshi",
+    "full name":"Sakshi Singh",
     age: 18,
+    [mySym]:"mykey1",
     location: "Indore",
     email: "sakshi@google.com",
     isLoggedIn: false,
-    
+    lastLogInDays: ["Monday", "Saturday"]
 }
+
+// console.log(jsUser.email);
+// console.log(jsUser["email"]);
+// console.log(jsUser["full name"]);
+// console.log(typeof(jsUser[mySym]));
+
+jsUser.email="sakshi2333@gmail.com"
+// Object.freeze(jsUser);
+jsUser.email="sakshi@microsoft.com";
+// console.log(jsUser);
+
+jsUser.greeting=function(){
+    console.log("Hello JS User");
+}
+
+jsUser.greetingTwo=function(){
+    console.log(`Hello JS User, ${this.name1}`);
+}
+
+console.log(jsUser.greeting());
+console.log(jsUser.greetingTwo());
---
+++ 02_basics/04_objects.js
@@ -0,0 +1,77 @@
+//Singleton or Objects using constructor
+
+// const tinderUser=new Object()
+const tinderUser={};
+tinderUser.id="123abc";
+tinderUser.name="lilly";
+tinderUser.isLoggedIn=false;
+
+// console.log(tinderUser);
+
+//Nesting of Objects
+
+const regularUser={
+    email: "some@gmail.com",
+    fullname: {
+        userFullName:{
+            firstName:"Sakshi",
+            lastName:"Singh"
+        }
+    }
+}
+
+// console.log(regularUser.fullname.userFullName.firstName);
+
+const obj1={1: "a", 2: "b"};
+const obj2={3: "a", 4:"b"};
+const obj4={5: "a", 6:"b"};
+
+// const obj3={obj1, obj2}
+// const obj3=Object.assign({}, obj1, obj2, obj4);
+
+const obj3={...obj1, ...obj2, ...obj4};
+// console.log(obj3);
+
+//Array of objects
+const users =[
+    {
+        id: 1,
+        email: "s@gmail.com"
+    },
+    {
+        id: 2,
+        email:"t@gmail.com"
+    },
+    {
+        id: 3,
+        email:"u@gmail.com"
+    }
+]
+
+users[1].email;
+// console.log(tinderUser);
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
+
+// console.log(tinderUser.hasOwnProperty('isLogged'));
+
+
+//+++++++++++ Objects Destructuring +++++
+
+const flower={
+
+    flowerName:"rose",
+    color:"Pink",
+    plantType:"shrub"
+}
+
+// (flower.color)
+
+const {flowerName: fn1}= flower
+
+// console.log(flowerName);
+console.log(fn1);
+
+
+// const navbar=()
