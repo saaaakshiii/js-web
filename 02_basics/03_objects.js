35aef322972f3e8ff8bb2da56bfd5771ffa61c17
saaaakshiii <147138497+saaaakshiii@users.noreply.github.com>
2024-10-10 05:39:49 +0000

    objects

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,12 @@
+// Singleton -object made from construtor
+// Object.create
+//Object Literals
+
+const jsUser={
+    name1: "Sakshi",
+    age: 18,
+    location: "Indore",
+    email: "sakshi@google.com",
+    isLoggedIn: false,
+    
+}
